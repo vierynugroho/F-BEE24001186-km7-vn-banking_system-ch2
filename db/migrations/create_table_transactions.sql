ced6f9cbb568121d569deb1b9641c53038caca28
Viery Nugroho <viery15102002@gmail.com>
2024-10-05 12:16:55 +0700

    fix(database migration): fix query for create type on trx table

--- db/migrations/create_table_transactions.sql
+++ db/migrations/create_table_transactions.sql
@@ -1,8 +1,8 @@
 DO 
 $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'transaction_type') THEN
-        CREATE TYPE transaction_type AS ENUM ('DEPOSIT', 'WITHDRAWAL', 'TRANSFER');
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname iLike 'TRANSACTION_TYPE') THEN
+        CREATE TYPE TRANSACTION_TYPE AS ENUM ('DEPOSIT', 'WITHDRAWAL', 'TRANSFER');
     END IF;
 END
 $$;

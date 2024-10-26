fee830c5a6174736509fdb022334b45cd4a9fa76
Viery Nugroho <viery15102002@gmail.com>
2024-10-26 12:06:34 +0700

    feat: implement auth middleware for users features

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { UsersController } from '../controllers/users.js';
+import authentication from '../middlewares/authentication.js';
 const router = express.Router();
 
-router.route('/').get(UsersController.getUsers);
-router.route('/:userId').get(UsersController.getUserById);
+router.route('/').get(authentication, UsersController.getUsers);
+router.route('/:userId').get(authentication, UsersController.getUserById);
 
 export default router;

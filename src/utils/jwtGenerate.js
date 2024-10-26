a7490d3c5054c3b773ab96206b1dd4c163a62f5f
Viery Nugroho <viery15102002@gmail.com>
2024-10-26 19:13:44 +0700

    docs: add example for forbidden response in all endpoint

--- src/utils/jwtGenerate.js
+++ src/utils/jwtGenerate.js
@@ -3,6 +3,7 @@ import jsonwebtoken from 'jsonwebtoken';
 const { sign } = jsonwebtoken;
 
 const generateJWT = (payload) => {
+  console.log(process.env.JWT_EXPIRED);
   return sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRED,
   });

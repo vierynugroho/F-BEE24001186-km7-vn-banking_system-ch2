676109e71fecf0a743658be2f7be4d4c30408346
Viery Nugroho <viery15102002@gmail.com>
2024-10-29 09:59:25 +0700

    test: add testing for users controller

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -74,10 +74,6 @@ export class UsersController {
       const page = parseInt(req.query.page) || 1;
       const limit = parseInt(req.query.limit) || 5;
 
-      if (isNaN(page) || isNaN(limit)) {
-        throw new ErrorHandler(400, 'page & limit must be a number');
-      }
-
       const offset = (page - 1) * limit;
       const pagination = {
         offset,
@@ -119,10 +115,7 @@ export class UsersController {
       }
 
       if (userLoggedIn.role != 'ADMIN' && userLoggedIn.id !== userID) {
-        throw new ErrorHandler(
-          403,
-          `you doesn't have and access for this data`,
-        );
+        throw new ErrorHandler(403, `you doesn't have an access for this data`);
       }
 
       const user = await UsersService.getUserById(userID);
